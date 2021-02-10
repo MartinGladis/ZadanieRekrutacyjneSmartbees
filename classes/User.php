4fee74d2031d54ef5fe8a6826f06f80a5d458e99
MartinGladis <mgladis92@gmail.com>
2021-02-10 15:09:28 +0100

    add reading information from json

--- classes/User.php
+++ classes/User.php
@@ -1,17 +1,54 @@
 <?php
-require "Adress.php";
+require "Address.php";
 require "Company.php";
+include "phpqrcode/qrlib.php"
 
 class User
 {
-    private $id;
-    private $name;
-    private $username;
-    private $email;
-    private Adress $address;
-    private $phone;
-    private $website;
+    private int $id;
+    private string $name;
+    private string $username;
+    private string $email;
+    public Address $adress;
+    private string $phone;
+    private string $website;
     private Company $company;
+
+    public function setPropertiesFromJson($jsonData)
+    {
+        $this->id = $jsonData["id"];
+        $this->name = $jsonData["name"];
+        $this->username = $jsonData["username"];
+        $this->email = $jsonData["email"];
+        $address = $jsonData["address"];
+        $geo = $address["geo"];
+        $this->address = new Address(
+            $address["street"], 
+            $address["suite"], 
+            $address["city"], 
+            $address["zipcode"], 
+            new Geo(
+                $geo["lat"],
+                $geo["lng"]
+            )
+        );
+        $this->phone = $jsonData["phone"];
+        $this->website = $jsonData["website"];
+        $company = $jsonData["company"];
+        $this->company = new Company(
+            $company["name"],
+            $company["catchPhrase"],
+            $company["bs"]
+        );
+    }
+
+    public function __construct(string $jsonUrl)
+    {
+        $json = file_get_contents($jsonUrl);
+        $jsonData = json_decode($json, true);
+
+        $this->setPropertiesFromJson($jsonData);
+    }
 
     public function getDomain()
     {
@@ -19,8 +56,8 @@ class User
         return $domain;
     }
 
-    public function setEmail($email)
+    public function getPersonData()
     {
-        $this->email = $email;
+
     }
 }
